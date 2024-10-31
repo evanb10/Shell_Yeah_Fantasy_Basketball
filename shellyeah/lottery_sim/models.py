e78a57e15968be6f4e07a70c3dc3cb0fef8f369f
evan <eablake153@gmail.com>
2024-10-30 17:34:22 -0700

    Lottery sim is now connected back into db

--- shellyeah/lottery_sim/models.py
+++ shellyeah/lottery_sim/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from players.models import Player
 
 # Create your models here.
 # class Player(models.Model):
@@ -14,47 +15,47 @@ from django.db import models
 
 #     def __str__(self):
 #       return f"{self.first_name} {self.last_name}"  
-# class Manager(models.Model):
-#   ''' Create Manager model that the database will be used to create the database.
-#       Manager will house any and all necessary information about league managers. '''
-#   manager_id = models.CharField(max_length=255,primary_key=True)
-#   team_name = models.CharField(max_length=255,null=True)
-#   display_name = models.CharField(max_length=255)
-#   record = models.CharField(max_length=255,null=True)
-#   wins = models.IntegerField(null=False,default=0)
-#   losses = models.IntegerField(null=False,default=0)
-#   points_for = models.IntegerField(null=False, default=0)
-#   points_against = models.IntegerField(null=False, default=0)
+class Manager(models.Model):
+  ''' Create Manager model that the database will be used to create the database.
+      Manager will house any and all necessary information about league managers. '''
+  manager_id = models.CharField(max_length=255,primary_key=True)
+  team_name = models.CharField(max_length=255,null=True)
+  display_name = models.CharField(max_length=255)
+  record = models.CharField(max_length=255,null=True)
+  wins = models.IntegerField(null=False,default=0)
+  losses = models.IntegerField(null=False,default=0)
+  points_for = models.IntegerField(null=False, default=0)
+  points_against = models.IntegerField(null=False, default=0)
 
-#   def __repr__(self) -> str:
-#       return self.__str__()
+  def __repr__(self) -> str:
+      return self.__str__()
 
-#   def __str__(self) -> str:
-#       return self.display_name
+  def __str__(self) -> str:
+      return self.display_name
 
-# class Roster(models.Model):
-#   ''' The roster table will house just the player ids and manager ids and will be used to track current fantasy rosters. '''
-#   manager_id = models.ForeignKey(Manager, on_delete=models.CASCADE, db_column='manager_id')
-#   player_id = models.ForeignKey(Player, on_delete=models.CASCADE, db_column='player_id')
+class Roster(models.Model):
+  ''' The roster table will house just the player ids and manager ids and will be used to track current fantasy rosters. '''
+  manager_id = models.ForeignKey(Manager, on_delete=models.CASCADE, db_column='manager_id')
+  player_id = models.ForeignKey(Player, on_delete=models.CASCADE, db_column='player_id')
 
-# class League(models.Model):
-#    ''' The League table will house necesary information for the current season's league. '''
-#    league_id = models.BigIntegerField(primary_key=True)
-#    num_of_teams = models.IntegerField()
-#    sport = models.CharField(max_length=25)
-#    league_name = models.CharField(max_length=255)
-#    previous_league_id = models.BigIntegerField()
-#    year = models.IntegerField()
+class League(models.Model):
+   ''' The League table will house necesary information for the current season's league. '''
+   league_id = models.BigIntegerField(primary_key=True)
+   num_of_teams = models.IntegerField()
+   sport = models.CharField(max_length=25)
+   league_name = models.CharField(max_length=255)
+   previous_league_id = models.BigIntegerField()
+   year = models.IntegerField()
 
-# class LeagueMembership(models.Model):
-#   """
-#   This model represents a membership of a Manager in a League.
-#   """
-#   manager = models.ForeignKey(Manager, on_delete=models.CASCADE)
-#   league = models.ForeignKey(League, on_delete=models.CASCADE)
-#   # You can add additional fields specific to memberships here (e.g., joined_date, role)
+class LeagueMembership(models.Model):
+  """
+  This model represents a membership of a Manager in a League.
+  """
+  manager = models.ForeignKey(Manager, on_delete=models.CASCADE)
+  league = models.ForeignKey(League, on_delete=models.CASCADE)
+  # You can add additional fields specific to memberships here (e.g., joined_date, role)
 
-#   class Meta:
-#     unique_together = (('manager', 'league'),)  # Enforces unique manager-league combination
+  class Meta:
+    unique_together = (('manager', 'league'),)  # Enforces unique manager-league combination
 
 
--- shellyeah/lottery_sim/views.py
+++ shellyeah/lottery_sim/views.py
@@ -16,16 +16,6 @@ from django.db.models import Subquery
 
 # Create your views here.
 
-# def player_list(request):
-#     # Query the database for all players
-#     players = Player.objects.all()
-    
-#     # Pass the players to the template
-#     return render(request, 'player_list.html', {'players': players})
-
-
-
-
 # def players(request):
 #     ''' Players Page '''
 #     # filter form to be displayed. 
@@ -107,112 +97,112 @@ from django.db.models import Subquery
        
 #     return render(request,'players.html',context)
 
-# def league_id(request):
-#     context = {}
-#     # if this is a POST request we need to process the form data
-#     if request.method == "POST":
-#         # create a form instance and populate it with data from the request:
-#         id_form = league_id_form.LeagueForm(request.POST)
+def league_id(request):
+    context = {}
+    # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        id_form = league_id_form.LeagueForm(request.POST)
 
-#         # check whether it's valid:
-#         if id_form.is_valid():
-#             # Get league id
-#             league_id = request.POST['league_id']
+        # check whether it's valid:
+        if id_form.is_valid():
+            # Get league id
+            league_id = request.POST['league_id']
             
-#             sleeper = sleeper_league(league_id)
-#             # Update league 
-#             sleeper.get_league_api()
-#             #Update managers
-#             sleeper.get_managers()
-#             # sleeper.clear_managers_table()
-#             sleeper.save_managers_to_database()
+            sleeper = sleeper_league(league_id)
+            # Update league 
+            sleeper.get_league_api()
+            #Update managers
+            sleeper.get_managers()
+            # sleeper.clear_managers_table()
+            sleeper.save_managers_to_database()
 
-#             #Update rosters
-#             sleeper.get_rosters()
-#             return redirect(reverse('get_odds', kwargs={'league_id': league_id}))
+            #Update rosters
+            sleeper.get_rosters()
+            return redirect(reverse('get_odds', kwargs={'league_id': league_id}))
 
-#             # Pull all information about a league given the provided league ID
+            # Pull all information about a league given the provided league ID
 
-#     # if a GET (or any other method) we'll create a blank form
-#     else:
-#         form = league_id_form.LeagueForm()
-#         context['form'] = form
-#     return render(request, 'league_id_form.html',context)
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = league_id_form.LeagueForm()
+        context['form'] = form
+    return render(request, 'league_id_form.html',context)
 
-# def get_odds(request,league_id):
-#     context = {}
-#     def odds(num):
-#         odds_dict = {
-#             12:[30,25,20,15,10,0,0,0,0,0,0,0],
-#             10:[40,30,20,10,0,0,0,0,0,0],
-#             8:[45,35,20,0,0,0,0,0],
-#             }
-#         return odds_dict[num]
+def get_odds(request,league_id):
+    context = {}
+    def odds(num):
+        odds_dict = {
+            12:[30,25,20,15,10,0,0,0,0,0,0,0],
+            10:[40,30,20,10,0,0,0,0,0,0],
+            8:[45,35,20,0,0,0,0,0],
+            }
+        return odds_dict[num]
     
-#     # Define a function to calculate wins from the record string
-#     def get_wins(record):
-#         if not record:
-#             return 0
-#         return record.count('W')
+    # Define a function to calculate wins from the record string
+    def get_wins(record):
+        if not record:
+            return 0
+        return record.count('W')
     
-#     def extract_percentage_values(data):
-#         percentage_values = []
-#         for key, value in data.items():
-#             if key.startswith('percentage_'):
-#                 clean_key = key.replace('percentage_', '')
-#                 percentage_values.append((clean_key, int(value)))        
-#         return percentage_values
+    def extract_percentage_values(data):
+        percentage_values = []
+        for key, value in data.items():
+            if key.startswith('percentage_'):
+                clean_key = key.replace('percentage_', '')
+                percentage_values.append((clean_key, int(value)))        
+        return percentage_values
 
-#     league_query = League.objects.filter(league_id=league_id).values().first()
-#     if league_query:
-#         # Prepare context data
-#         memebership_query = LeagueMembership.objects.filter(league_id=league_id).values_list('manager_id',flat=True)
-#         print(memebership_query)
-#         teams = Manager.objects.filter(manager_id__in=Subquery(memebership_query))
-#         print(teams)
+    league_query = League.objects.filter(league_id=league_id).values().first()
+    if league_query:
+        # Prepare context data
+        memebership_query = LeagueMembership.objects.filter(league_id=league_id).values_list('manager_id',flat=True)
+        print(memebership_query)
+        teams = Manager.objects.filter(manager_id__in=Subquery(memebership_query))
+        print(teams)
 
-#         # teams = Manager.objects.filter(league_id=league_id)
-#         league_ = script_league()
-#         for manager in teams:
-#             manager_name = manager.display_name
-#             # odds_ = teams['percentage_' + manager_name]
-#             wins = manager.wins
-#             losses = manager.losses
-#             points_for = manager.points_for
-#             points_against = manager.points_against
+        # teams = Manager.objects.filter(league_id=league_id)
+        league_ = script_league()
+        for manager in teams:
+            manager_name = manager.display_name
+            # odds_ = teams['percentage_' + manager_name]
+            wins = manager.wins
+            losses = manager.losses
+            points_for = manager.points_for
+            points_against = manager.points_against
 
 
-#             new_team = script_team(manager_name, 0, wins, losses, points_for, points_against)
-#             league_.addTeam(new_team)
+            new_team = script_team(manager_name, 0, wins, losses, points_for, points_against)
+            league_.addTeam(new_team)
     
-#         league_.sort_teams()
-#         odds_values = odds(league_query['num_of_teams'])
-#         for idx,team in enumerate(league_.teams,start=1):
-#             team.odds = odds_values[-idx]
-#         # sorted_list = sorted(teams, key=lambda x: (get_wins(x[1]), x[0]))
-#         # zipped_data = zip([team[0] for team in sorted_list], odds_values)
-#         # teams_list = list(zipped_data)
-#         context['teams'] = league_.teams
-#     else:
-#         return HttpResponseBadRequest("League not found.")
+        league_.sort_teams()
+        odds_values = odds(league_query['num_of_teams'])
+        for idx,team in enumerate(league_.teams,start=1):
+            team.odds = odds_values[-idx]
+        # sorted_list = sorted(teams, key=lambda x: (get_wins(x[1]), x[0]))
+        # zipped_data = zip([team[0] for team in sorted_list], odds_values)
+        # teams_list = list(zipped_data)
+        context['teams'] = league_.teams
+    else:
+        return HttpResponseBadRequest("League not found.")
 
-#     if request.method == "POST":
-#         # teams = extract_percentage_values(request.POST)
-#         form_odds = odds_form.PercentageAllocationForm(request.POST or None, teams=league_.teams)
+    if request.method == "POST":
+        # teams = extract_percentage_values(request.POST)
+        form_odds = odds_form.PercentageAllocationForm(request.POST or None, teams=league_.teams)
 
-#         if form_odds.is_valid():
-#             cleaned_data = form_odds.cleaned_data
-#             # Process cleaned data
-#             return redirect(reverse('lottery_results', kwargs={'league_id': league_id})+ f'?cleaned_data={cleaned_data}')
-#         else:
-#             context['form'] = form_odds  # Pass the form with errors to the template
-#             return render(request, 'lottery_sim.html', context)
+        if form_odds.is_valid():
+            cleaned_data = form_odds.cleaned_data
+            # Process cleaned data
+            return redirect(reverse('lottery_results', kwargs={'league_id': league_id})+ f'?cleaned_data={cleaned_data}')
+        else:
+            context['form'] = form_odds  # Pass the form with errors to the template
+            return render(request, 'lottery_sim.html', context)
 
-#     else:
+    else:
 
-#             form_odds = odds_form.PercentageAllocationForm(teams=league_.teams[::-1])
-#             context['form'] = form_odds
-#             return render(request, 'lottery_sim.html', context)
+            form_odds = odds_form.PercentageAllocationForm(teams=league_.teams[::-1])
+            context['form'] = form_odds
+            return render(request, 'lottery_sim.html', context)
 
 
 
